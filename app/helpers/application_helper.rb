5f1da772094056eb161abe9695e27abaa1ff79f4
内藤祐介 <yusuke@naitouyukenoAir.lan>
2024-11-19 11:40:08 +0900

    日付のフォーマットの作成s

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def format_date(date)
+    date.strftime("%Y年%m月%d日")
+  end
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ApplicationRecord
   has_many :transaction_categories
   has_many :categories, through: :transaction_categories
+
+  validate :transaction_date, presence, true
 end
