b8a3377d32b37f811d0baa542c28123c301fd3e5
内藤祐介 <yusuke@naitouyukenoAir.lan>
2024-11-19 12:49:33 +0900

    収支の合計を計算し表示できるように修正

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,4 @@
 class Transaction < ApplicationRecord
   has_many :transaction_categories
   has_many :categories, through: :transaction_categories
-
-  validate :transaction_date, presence, true
 end
