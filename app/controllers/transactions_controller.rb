bf4d62c0fa9e3762dfc263d99ce92194fda9e6c7
内藤祐介 <yusuke@naitouyukenoAir.lan>
2024-11-20 16:56:53 +0900

    月ごとの収支を収入、支出、合計のそれぞれに分けて棒グラフで算出する機能を実装

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -43,6 +43,37 @@ class TransactionsController < ApplicationController
     flash[:notice] = "削除が成功しました"
   end
 
+  def monthly
+    # 横軸を1月から12月の固定ラベルで設定
+    @labels = (1..12).map { |month| "#{month}月" }
+
+    # 収支データを月ごとに集計
+    income_date = Transaction.where(transaction_type: 'income').group_by_month(:transaction_date, format: "%m", range: Date.current.beginning_of_year..Date.current.end_of_year).sum(:amount)
+    expense_date = Transaction.where(transaction_type: 'expense').group_by_month(:transaction_date, format: "%m", range: Date.current.beginning_of_year..Date.current.end_of_year).sum(:amount)
+    #　収支の合計金額を格納するための新しいハッシュ
+    @total_values = {}
+    #　収支の合計を計算
+    income_date.each do |key, income_value|
+      expense_value = expense_date[key] || 0
+      @total_values[key] = income_value - expense_value
+    end
+    # データをラベルの順序に合わせて並べ替え
+    @income_values = @labels.map do |label|
+      month_number = label.gsub("月", "").to_i
+      income_date[month_number.to_s] || 0 # 該当データがない月は0を設定
+    end
+
+    @expense_values = @labels.map do |label|
+      month_number = label.gsub("月", "").to_i
+      expense_date[month_number.to_s] || 0 # 該当データがない月は0を設定
+    end
+
+    @total_values = @labels.map do |label|
+      month_number = label.gsub("月", "").to_i
+      @total_values[month_number.to_s] || 0
+    end
+  end
+
   def transaction_params
     params.require(:transaction).permit(:amount, :transaction_type, :transaction_date, :note)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
+  get 'transactions/monthly', to: 'transactions#monthly', as: 'monthly_transactions'
   resources :transactions
 end
